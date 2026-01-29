7dee7c747f5c63d40b56ef2b7680c902a466713f
sualih <sualihmohammed53@gmail.com>
2026-01-29 12:22:28 +0300

    Solved deployment error

--- config/site.ts
+++ config/site.ts
@@ -7,16 +7,16 @@ export const siteConfig = {
   url: "http://localhost:3000",
   links: {
     github: "https://github.com/sualihh",
-    templateRepo: "https://github.com/sualihh/minimal-next-portfolio",
+    templateRepo: "https://github.com/sualihh/portfolio",
   },
-  // ogImage:
-  //   "https://res.cloudinary.com/dvt5vkfwz/image/upload/v1767384721/naman_portfolio_og_image.png",
-  // iconIco:
-  //   "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
-  // logoIcon:
-    // "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
+  ogImage:
+    "https://res.cloudinary.com/dsfd6kdck/image/upload/v1769677932/Screenshot_2026-01-29_114628_wh9jof.png",
+  iconIco:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
+  logoIcon:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
   keywords: [
-    "Naman Barkiya",
+    "Sualih Mohammed",
     "Applied AI Engineer",
     "AI Engineer",
     "Software Engineer",
